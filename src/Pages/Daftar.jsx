5d7c391f72430aa01e3138ddc7729c01e4ce965d
dortea ribka tadete <dortearibkatadete17.com>
2024-11-22 21:48:19 +0800

    remove navbar&footer from daftar

--- src/Pages/Daftar.jsx
+++ src/Pages/Daftar.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import Navbar from '../Components/Navbar'
-import Footer from '../Components/Footer'
 import SectionDaftar from '../Sections/Auth/SectionDaftar'
 
 export default function Daftar() {
   return (
     <>
-      <Navbar/>
       <SectionDaftar/>
-      <Footer/>
     </>
   )
 }
