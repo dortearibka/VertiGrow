fec6b78563d6fd78159e77bff5b9c41723708946
Dortea Ribka Tadete <153445261+dortearibka@users.noreply.github.com>
2024-11-22 20:35:47 +0800

    Update Daftar.jsx

--- src/Pages/Daftar.jsx
+++ src/Pages/Daftar.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Navbar from '../Components/Navbar'
 import SectionDaftar from '../Sections/Login/SectionDaftar'
 import Footer from '../Components/Footer'
-=======
 import SectionDaftar from '../Sections/Auth/SectionDaftar'
 
 export default function Daftar() {
